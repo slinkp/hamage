b463f6c506ab31f2f2e3ccd56e93dcdd57667f60
Paul Winkler <slinkp@gmail.com>
2012-06-09 18:03:31 -0400

    Tests of the expression phrase generator

--- hamage/captcha/expression.py
+++ hamage/captcha/expression.py
@@ -16,64 +16,81 @@
 
 import random
 
-from trac.core import *
-from trac.config import *
 from trac.util.html import html, Markup
-from tracspamfilter.captcha import ICaptchaMethod
+
+operation_names = {'*': 'multiplied by', '-': 'minus', '+': 'plus'}
+
+numerals = (
+    'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+    'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
+    'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
+    'nineteen' )
+
+tens = ('twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+        'eighty', 'ninety')
 
 
-class ExpressionCaptcha(Component):
+def _interleave(list1, list2):
+    """This creates a list of [list1[0], list2[0], list1[1], list2[1], ...]
+
+    If length is unequal, stop at length of shorter list.
+    """
+    return list(sum(zip(list1, list2), ()))
+
+
+def _make_expression(terms, operations):
+    assert len(operations) == len(terms) - 1
+    # To interleave we want the same length.
+    expression = _interleave(terms, operations + ['unused'])[:-1]
+    expression = eval(compile(' '.join(expression), 'captcha_eval', 'eval'))
+    return expression
+
+
+def _humanize_expression(terms, operations):
+    expression = _make_expression(terms, operations)
+    # To interleave we want the same length.
+    op_names = [operation_names[o] for o in operations] + ['unused']
+    human = _interleave([_humanise(int(t)) for t in terms], op_names)[:-1]
+    return expression, human
+
+def _humanise(value):
+    if value < 20:
+        return numerals[value]
+    english = tens[value / 10 - 2]
+    if value % 10:
+        english += ' ' + numerals[value % 10]
+    return english
+
+
+class ExpressionCaptcha(object):
     """captcha in the form of a human readable numeric expression
-    
+
     Initial implementation by sergeych@tancher.com.
     """
 
-    implements(ICaptchaMethod)
+    is_captcha = True
 
-    terms = IntOption('spam-filter', 'captcha_expression_terms', 3,
-            """Number of terms in numeric CAPTCHA expression.""")
-
-    ceiling = IntOption('spam-filter', 'captcha_expression_ceiling', 10,
-            """Maximum value of individual terms in numeric CAPTCHA
-            expression.""")
-
-    operations = {'*': 'multiplied by', '-': 'minus', '+': 'plus'}
-
-    numerals = ('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
-                'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
-                'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
-                'nineteen' )
-
-    tens = ('twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
-            'eighty', 'ninety')
-
-    # ICaptchaMethod methods
+    def __init__(self, config):
+        self.config = config
+        # Number of terms in numeric CAPTCHA expression.
+        self.terms = config.get('captcha_expression_terms', 3)
+        # Maximum value of individual terms in numeric CAPTCHA expression.
+        self.ceiling = config.get('captcha_expression_ceiling', 10)
+        if self.ceiling > 100:
+            raise ValueError('Numeric captcha can not represent numbers > 100')
 
     def generate_captcha(self, req):
-        if self.ceiling > 100:
-            raise TracError('Numeric captcha can not represent numbers > 100')
         terms = [str(random.randrange(0, self.ceiling)) 
                  for i in xrange(self.terms)]
-        operations = [random.choice(self.operations.keys()) 
-                      for i in xrange(self.terms)]
-        expression = sum(zip(terms, operations), ())[:-1]
-        expression = eval(compile(' '.join(expression), 'captcha_eval', 'eval'))
-        human = sum(zip([self._humanise(int(t)) for t in terms],
-                        [self.operations[o] for o in operations]), ())[:-1]
+        operations = [random.choice(operation_names.keys()) 
+                      for i in xrange(self.terms -1)]
+        expression, human = _humanize_expression(terms, operations)
         return (expression, html.blockquote(' '.join(map(str, human))))
 
     def verify_captcha(self, req):
+        # Um, this should do something.
         return False
 
     def is_usable(self, req):
         return True
 
-    # Internal methods
-    
-    def _humanise(self, value):
-        if value < 20:
-            return self.numerals[value]
-        english = self.tens[value / 10 - 2]
-        if value % 10:
-            english += ' ' + self.numerals[value % 10]
-        return english
